17e1ea9b7b374caef822f51e4e71a2639f09a5f9
Mette <tobiasen_1@hotmail.com>
2018-03-14 17:48:58 +0100

    tilføjet Exit metode til WeightFunction klassen

--- CDIO2/src/ful/WeightFunction.java
+++ CDIO2/src/ful/WeightFunction.java
@@ -106,4 +106,27 @@ public class WeightFunction {
 	        in.close();
 	        pingSocket.close();
 	        }
+	        
+	        public void ExitWeight() throws IOException {
+	        	
+//	        	public static void main(String[] args) throws IOException{
+//       		
+//	        		Socket pingSocket = null;
+//	    	        PrintWriter out = null;
+//	    	        BufferedReader in = null;
+	    	        
+	        	try {
+	        		pingSocket = new Socket("127.0.0.1", 8000);
+	        		System.out.println("Connected to weight simulator");
+	        		out = new PrintWriter(pingSocket.getOutputStream(), true);
+	        		in = new BufferedReader(new InputStreamReader(pingSocket.getInputStream()));
+	        	} catch(IOException e) {
+	        		return;		
+	        }
+	        out.println("Q crlf");
+	        System.out.println(in.readLine());
+	        out.close();
+	        in.close();
+	        pingSocket.close();
+	        }
 }
